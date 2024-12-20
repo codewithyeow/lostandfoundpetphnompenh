cae8f0e0d21fe6e2a3e37e9aaddf1b401daa6ce3
codewithyeow <pannsreyoun5@gmail.com>
2024-12-20 23:50:44 +0700

    updated'

--- .next/static/chunks/src_77e307._.js
+++ .next/static/chunks/src_77e307._.js
@@ -145,7 +145,7 @@ function Section1() {
                                     }, this),
                                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
                                         className: "text-xl mt-4 text-center",
-                                        children: "Petco Love Lost is a free and easy way to search 200K+ lost and found pets to help them return home."
+                                        children: "Lost and Found is a free and easy way to search 200K+ lost and found pets to help them return home."
                                     }, void 0, false, {
                                         fileName: "[project]/src/components/home/section1/Section1.tsx",
                                         lineNumber: 70,
